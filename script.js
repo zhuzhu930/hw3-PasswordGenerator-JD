50bdd558fa7f9b909d2c7a69fc3056171e448ed0
Jessie Doherty <zhuzhu930@yahoo.com>
2022-03-19 18:11:46 -0500

    Modified HTML, CSS and JS

--- script.js
+++ script.js
@@ -1,66 +1,54 @@
 
-//button id="generate"
-//textarea id="password"
+//Define variables.
+const characterAmountNumber = document.getElementById('characterAmountNumber')
+const includeUppercaseEl = document.getElementById('includeUppercase')
+const includeNumbersEl = document.getElementById('includeNumbers')
+const includeSymbolsEl = document.getElementById('includeSymbols')
+const form = document.getElementById('passwordGeneratorForm')
+const passwordDisplay = document.getElementById('passwordDisplay')
 
-// Approach 1: Make a string consist of Alphabets(lowercase and uppercase), Numbers and Special Characters. the we will use Math.random() and Math.floor() method to generate a number in between 0 and l-1(where l is length of string). To get the character of the string of a particular index we can use .charAt() method. This will keep concatenating the random character from the string until the password of the desired length is obtained.
+//setting up character codes and generate code array. 
+const uppercaseCodes = arrayFromLowToHigh(65, 90)
+const lowercaseCodes = arrayFromLowToHigh(97, 122)
+const numberCodes = arrayFromLowToHigh(48, 57)
+const symbolsCodes = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
 
-var textarea = document.getElementById("password");
-var generateBtn = document.querySelector("#generate");
-  
-/* Function to generate combination of password */
-function generatePassword() {
-    var passwordText = '';
-    var stringList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 
-              'abcdefghijklmnopqrstuvwxyz0123456789@#$';
-              
-    for (i = 1; i <= 8; i++) {
-      var char = Math.floor(Math.random() * stringList.length + 1);
-                  
-      passwordText += stringList.charAt(char)
-    }
-              
-    return passwordText;
+function arrayFromLowToHigh(low, high) {
+  const array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i)
   }
-
-// function generateRun() {
-//     textarea.innerHTML = generatePassword();
-//   }
-
-generateBtn.addEventListener("click", generateRun);
-
-function generateRun() {
-  textarea.innerHTML = generatePassword();
+  return array
 }
 
-//   // Assignment Code
-// var generateBtn = document.querySelector("#generate");
+//set up event listener:
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const characterAmount = characterAmountNumber.value
+  const includeUppercase = includeUppercaseEl.checked;
+  const includeNumbers = includeNumbersEl.checked;
+  const includeSymbols = includeSymbolsEl.checked;
+  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols);
+  passwordDisplay.textContent = password;
+})
 
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+//define functions: 
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+  let charCodes = lowercaseCodes
+  if (includeUppercase) charCodes = charCodes.concat(uppercaseCodes)
+  if (includeNumbers) charCodes = charCodes.concat(numberCodes)
+  if (includeSymbols) charCodes = charCodes.concat(symbolsCodes)
 
-//   passwordText.value = password;
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
+  const passwordText = []
+  for (let i = 0; i < characterAmount; i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordText.push(String.fromCharCode(characterCode))
+  }
+  return passwordText.join('')
+}
